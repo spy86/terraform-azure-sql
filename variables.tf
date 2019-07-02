18e8271b941cd79f0c5f667809be1c7fd6b772eb
spy8681 <snajperm14@gmail.com>
2019-07-02 20:57:51 +0200

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+    default = "dev-"
+    description = "Prefix which are used for all resources"
+  
+}
+
+variable "location" {
+    default = "West Europe"
+    description = "Location which are used for all resources"
+  
+}
